d80f3ddadc72ecddbf611a2dd7611709a8395d13
GuggerJoel <joel@thimoo.ch>
2015-04-29 20:52:43 +0200

    app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ mongoose.connect(config.db);
 var db = mongoose.connection;
 db.on('error', function () {
   throw new Error('unable to connect to database at ' + config.db);
-});
+})
 
 
 var models = glob.sync(config.rootPath + '/app/models/*.js');
@@ -24,11 +24,11 @@ app.use(express.static('public'))
 
 http = require('http').Server(app)
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:3000')
 })
 
 require('./app/sockets/socket')(app, http)
 require('./config/express')(app, config)
 
 var GameCore = require('./app/services/gameCore')
-setInterval(GameCore.updateSectorsInfluence, 60000)
+setInterval(GameCore.updateSectorsInfluence, 3600000)
